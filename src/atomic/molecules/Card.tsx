8c0d0950d741a84fe6df9701e4b577e4ef985cfd
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-11-19 12:47:45 -0300

    feat: creating card and add to home

---
+++ src/atomic/molecules/Card.tsx
@@ -0,0 +1,33 @@
+import { Box, Image, Text } from 'native-base';
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { CardProps } from '../../page/Home';
+import { TextAlpha50 } from '../atoms/TextAlpha50';
+
+
+export function Card({
+    id,
+    image,
+    price,
+    model,
+    title
+}: CardProps) {
+    return <TouchableOpacity 
+        style={{padding: 5, width: '50%', height: 240, opacity: 0.9}} 
+        onPress={() => {}}>
+            <Box borderRadius="20px"
+                p="4" flex="1" shadow="5" bg="primary.50:alpha.9"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Box>
+                    <Image src={image} width="121px" h="80px" alt="image" resizeMode="contain" />
+                    <Box mt="17px">
+                        <TextAlpha50 text={title} />
+                        <Text fontSize="md" bold color="white">{model}</Text>
+                        <TextAlpha50 text={`RS ${price}`} />
+                    </Box>
+                </Box> 
+            </Box>
+    </TouchableOpacity>
+}
--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box } from 'native-base';
 import { DetailBackground } from '../../atomic/atoms/DetailBackground';
 import { MainBanner } from '../../atomic/molecules';
+import { Card } from '../../atomic/molecules/Card';
+import api from '../../service/api';
+import { FlatList } from 'react-native';
+
+
+export type CardProps = {
+    title: string;
+    price: number;
+    model: string;
+    image: string;
+    id: string
+}
 
 export function Home() {
+
+    const [equipments, setEquipments] = useState<CardProps[]>([])
+
+    useEffect(() => {
+        async function getEquipments() {
+            try {
+                const { data } = await api.get('equipments')
+                setEquipments(data)
+
+            } catch(err) {
+                console.log(err)
+            }
+        }
+
+        getEquipments()
+    }, [])
+
     return <Box flex="1" padding="20px" pt="-10px" position="relative">
         <DetailBackground />
-        <MainBanner />
+        <FlatList
+            ListHeaderComponent={() => <MainBanner />}
+            showsVerticalScrollIndicator={false}
+            numColumns={2}
+            keyExtractor={item => item.id}
+            data={equipments}
+            renderItem={({item: equipment}) => <Card image={equipment.image} id={equipment.id}
+             model={equipment.model} price={equipment.price} title={equipment.title}/>}
+         />
     </Box>
 }
