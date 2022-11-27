b39ade4832e226b57a1a5b27a54675b69c1c6d96
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-11-27 08:21:59 -0300

    refactor: adjustes on mainBanner

--- src/atomic/molecules/index.tsx
+++ src/atomic/molecules/index.tsx
@@ -6,7 +6,7 @@ import slider from '../../img/slider.png'
 import bike from '../../img/bike.png'
 
 export function MainBanner() {
-    return <Box w="100%" h="300" mt="60px">
+    return <Box w="100%" h="300" mt="-6">
         <ImageBackground source={slider} style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} resizeMode="contain">
             <Image source={bike} alt="image of a bike" />
         </ImageBackground>
