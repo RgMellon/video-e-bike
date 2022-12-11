f45e0b2f5b671c26aa8e8d814752d77a9800ea43
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-12-11 17:15:23 -0300

    refactor: adjustes on header component

--- src/atomic/molecules/Header.tsx
+++ src/atomic/molecules/Header.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
-import { Box, Icon, Text } from 'native-base';
-import { AntDesign } from '@expo/vector-icons'
-import { GradientSquareButton } from '../atoms/GradientSquareButton';
+import React from "react";
+import { Box, Icon, Text } from "native-base";
+import { AntDesign } from "@expo/vector-icons";
+import { SquareButton } from "../atoms/SquareButton";
 
-type Props = { 
-    text: string
-}
+type Props = {
+    text: string;
+};
 
-export function Header({text} : Props) {
-    return <Box h="100px" alignItems="center" mt="36" flexDirection="row" justifyContent="space-between">
-        <Text fontSize="lg" fontWeight="bold" color="white">{text}</Text>
+export function Header({ text }: Props) {
+    return (
+        <Box
+            h="100px"
+            alignItems="center"
+            mt="36"
+            flexDirection="row"
+            justifyContent="space-between"
+        >
+            <Text fontSize="lg" fontWeight="bold" color="white">
+                {text}
+            </Text>
 
-        <GradientSquareButton>
-            <Icon as={AntDesign} name="search1" color="white" />
-        </GradientSquareButton>
-    </Box>
+            <SquareButton>
+                <Icon as={AntDesign} name="search1" color="white" />
+            </SquareButton>
+        </Box>
+    );
 }
