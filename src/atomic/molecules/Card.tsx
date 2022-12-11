e6ba40f4a620c0b7e999c1d47b7c81daa3bd8bba
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-12-11 17:13:32 -0300

    feat: add feat of click on card

--- src/atomic/molecules/Card.tsx
+++ src/atomic/molecules/Card.tsx
@@ -1,33 +1,51 @@
-import { Box, Image, Text } from 'native-base';
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
-import { CardProps } from '../../page/Home';
-import { TextAlpha50 } from '../atoms/TextAlpha50';
+import { Box, Image, Text } from "native-base";
+import React from "react";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { CardProps } from "../../page/Home";
+import { TextAlpha50 } from "../atoms/TextAlpha50";
 
+type Props = CardProps & TouchableOpacityProps;
 
 export function Card({
     id,
     image,
     price,
     model,
-    title
-}: CardProps) {
-    return <TouchableOpacity 
-        style={{padding: 5, width: '50%', height: 240, opacity: 0.9}} 
-        onPress={() => {}}>
-            <Box borderRadius="20px"
-                p="4" flex="1" shadow="5" bg="primary.50:alpha.9"
+    title,
+    handleRedirect,
+    ...rest
+}: Props) {
+    return (
+        <TouchableOpacity
+            style={{ padding: 5, width: "50%", height: 240, opacity: 0.9 }}
+            {...rest}
+        >
+            <Box
+                borderRadius="20px"
+                p="4"
+                flex="1"
+                shadow="5"
+                bg="primary.50:alpha.9"
                 justifyContent="center"
                 alignItems="center"
             >
                 <Box>
-                    <Image src={image} width="121px" h="80px" alt="image" resizeMode="contain" />
+                    <Image
+                        src={image}
+                        width="121px"
+                        h="80px"
+                        alt="image"
+                        resizeMode="contain"
+                    />
                     <Box mt="17px">
                         <TextAlpha50 text={title} />
-                        <Text fontSize="md" bold color="white">{model}</Text>
+                        <Text fontSize="md" bold color="white">
+                            {model}
+                        </Text>
                         <TextAlpha50 text={`RS ${price}`} />
                     </Box>
-                </Box> 
+                </Box>
             </Box>
-    </TouchableOpacity>
+        </TouchableOpacity>
+    );
 }
