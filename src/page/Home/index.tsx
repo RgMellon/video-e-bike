e4c17207f224ad052de9890855535bd68cbae317
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-12-11 17:16:20 -0300

    feat: add navigate feat when card is clicked

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,54 +1,75 @@
-import React, { useEffect, useState } from 'react';
-import { Box } from 'native-base';
-import { DetailBackground } from '../../atomic/atoms/DetailBackground';
-import { MainBanner } from '../../atomic/molecules';
-import { Card } from '../../atomic/molecules/Card';
-import api from '../../service/api';
-import { FlatList } from 'react-native';
-import { Header } from '../../atomic/molecules/Header';
-
+import React, { useEffect, useState } from "react";
+import { Box } from "native-base";
+import { DetailBackground } from "../../atomic/atoms/DetailBackground";
+import { MainBanner } from "../../atomic/molecules/MainBanner";
+import { Card } from "../../atomic/molecules/Card";
+import api from "../../service/api";
+import { FlatList } from "react-native";
+import { Header } from "../../atomic/molecules/Header";
+import { CategoryList } from "../../atomic/organism/CategoryList";
+import { useNavigation } from "@react-navigation/native";
 
 export type CardProps = {
     title: string;
     price: number;
     model: string;
     image: string;
-    id: string
-}
+    id: string;
+    handleRedirect?: (id: string) => void;
+};
 
 export function Home() {
+    const { navigate } = useNavigation();
 
-    const [equipments, setEquipments] = useState<CardProps[]>([])
+    const [equipments, setEquipments] = useState<CardProps[]>([]);
 
     useEffect(() => {
         async function getEquipments() {
             try {
-                const { data } = await api.get('equipments')
-                setEquipments(data)
-
-            } catch(err) {
-                console.log(err)
+                const { data } = await api.get("equipments");
+                setEquipments(data);
+            } catch (err) {
+                console.log(err);
             }
         }
 
-        getEquipments()
-    }, [])
+        getEquipments();
+    }, []);
 
-    return <Box flex="1" padding="20px" pt="-10px" position="relative">
-        <DetailBackground />
-        <FlatList
-            ListHeaderComponent={() => 
-                <>
-                    <Header text='Choose Your Bike'/>
-                    <MainBanner />
-                </>
-            }
-            showsVerticalScrollIndicator={false}
-            numColumns={2}
-            keyExtractor={item => item.id}
-            data={equipments}
-            renderItem={({item: equipment}) => <Card image={equipment.image} id={equipment.id}
-             model={equipment.model} price={equipment.price} title={equipment.title}/>}
-         />
-    </Box>
+    function handleRedirect(id: string) {
+        navigate("Detail", {
+            equipmentId: id,
+        });
+    }
+
+    return (
+        <Box flex="1" padding="20px" pt="-10px" position="relative">
+            <DetailBackground />
+            <FlatList
+                ListHeaderComponent={() => (
+                    <>
+                        <Header text="Choose Your Bike" />
+                        <MainBanner />
+                        <CategoryList />
+                    </>
+                )}
+                showsVerticalScrollIndicator={false}
+                numColumns={2}
+                keyExtractor={(item) => item.id}
+                data={equipments}
+                renderItem={({ item: equipment }) => (
+                    <Card
+                        onPress={() => {
+                            handleRedirect(equipment.id);
+                        }}
+                        image={equipment.image}
+                        id={equipment.id}
+                        model={equipment.model}
+                        price={equipment.price}
+                        title={equipment.title}
+                    />
+                )}
+            />
+        </Box>
+    );
 }
