9a52a207ba6e9a8ca0266e79ae8910d3ee87c8d1
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2023-01-21 12:05:34 -0300

    feat: add animation

--- src/page/Detail/index.tsx
+++ src/page/Detail/index.tsx
@@ -4,6 +4,13 @@ import { useRoute } from "@react-navigation/native";
 import api from "../../service/api";
 import { CardProps } from "../Home";
 
+import Animated, {
+    Easing,
+    useAnimatedStyle,
+    useSharedValue,
+    withTiming,
+} from "react-native-reanimated";
+
 import rectangle from "../../img/rectangle.png";
 import { ImagePreview } from "../../atomic/atoms/ImagePreview";
 import { ButtonDetail } from "../../atomic/atoms/ButtonDetail";
@@ -14,9 +21,12 @@ type RouteParams = {
 
 export function Detail() {
     const route = useRoute();
-    const { equipmentId } = route.params as RouteParams;
 
+    const { equipmentId } = route.params as RouteParams;
     const [equipment, setEquipment] = useState<CardProps>({} as CardProps);
+    const [showDescription, setShowDescription] = useState(true);
+
+    const cardOffset = useSharedValue(460);
 
     useEffect(() => {
         async function getEquipmentById() {
@@ -31,6 +41,35 @@ export function Detail() {
 
         getEquipmentById();
     }, []);
+
+    function handleToggleFooter() {
+        setShowDescription((old) => !old);
+    }
+
+    function handleDescription() {
+        handleToggleFooter();
+
+        if (showDescription) {
+            cardOffset.value = withTiming(1, {
+                duration: 500,
+                easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+            });
+        } else {
+            cardOffset.value = withTiming(460, {
+                duration: 500,
+            });
+        }
+    }
+
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {
+                    translateY: cardOffset.value,
+                },
+            ],
+        };
+    });
 
     return (
         <>
@@ -50,13 +89,13 @@ export function Detail() {
                 />
 
                 <ImagePreview
-                    showDescription={false}
+                    showDescription={showDescription}
                     image={equipment.image}
                     alt={equipment.title}
                 />
             </Box>
 
-            {false ? (
+            {showDescription ? (
                 <Box
                     bg="primary.100"
                     justifyContent="center"
@@ -66,67 +105,81 @@ export function Detail() {
                     h="110px"
                 >
                     <HStack w="100%" justifyContent="space-between">
-                        <ButtonDetail title="Description" />
+                        <ButtonDetail
+                            title="Description"
+                            onPress={handleDescription}
+                        />
 
                         <ButtonDetail title="Specification" />
                     </HStack>
                 </Box>
             ) : (
-                <Box bg="primary.100" borderTopRadius={30} shadow={5} h="460px">
-                    <Box pl="7" p="7" pt="7">
-                        <HStack w="100%" justifyContent="space-between">
-                            <ButtonDetail title="Description" />
-
-                            <ButtonDetail title="Specification" />
-                        </HStack>
-
-                        <Box mt="30">
-                            <Text bold color="white" fontSize="17">
-                                {equipment.title}
-                            </Text>
-
-                            <Text color="white" opacity="0.6" mt="8">
-                                The LR01 uses the same design as the most iconic
-                                bikes from PEUGEOT Cycles' 130-year history and
-                                combines it with agile, dynamic performance
-                                that's perfectly suited to navigating today's
-                                cities. As well as a lugged steel frame and
-                                iconic PEUGEOT black-and-white chequer design,
-                                this city bike also features a 16-speed Shimano
-                                Claris drivetrain.
-                            </Text>
-                        </Box>
-                    </Box>
-                    <HStack
-                        bg="primary.200"
+                <Animated.View style={animatedStyle}>
+                    <Box
+                        bg="primary.100"
                         borderTopRadius={30}
-                        justifyContent="space-between"
                         shadow={5}
-                        h={100}
-                        w={"100%"}
-                        mt="30"
-                        alignItems="center"
-                        p="7"
+                        h={showDescription ? "110px" : "460px"}
                     >
-                        <Text bold color="ocean.200" fontSize="17">
-                            {equipment.price}
-                        </Text>
+                        <Box pl="7" p="7" pt="7">
+                            <HStack w="100%" justifyContent="space-between">
+                                <ButtonDetail
+                                    title="Description"
+                                    onPress={handleDescription}
+                                />
 
-                        <Button
-                            w="149"
-                            h="43"
-                            justifyContent="center"
-                            alignItems="center"
-                            borderRadius="10px"
+                                <ButtonDetail title="Specification" />
+                            </HStack>
+
+                            <Box mt="30">
+                                <Text bold color="white" fontSize="17">
+                                    {equipment.title}
+                                </Text>
+
+                                <Text color="white" opacity="0.6" mt="8">
+                                    The LR01 uses the same design as the most
+                                    iconic bikes from PEUGEOT Cycles' 130-year
+                                    history and combines it with agile, dynamic
+                                    performance that's perfectly suited to
+                                    navigating today's cities. As well as a
+                                    lugged steel frame and iconic PEUGEOT
+                                    black-and-white chequer design, this city
+                                    bike also features a 16-speed Shimano Claris
+                                    drivetrain.
+                                </Text>
+                            </Box>
+                        </Box>
+                        <HStack
+                            bg="primary.200"
+                            borderTopRadius={30}
+                            justifyContent="space-between"
                             shadow={5}
-                            bg="ocean.200"
-                            _pressed={{ bgColor: "ocean.100" }}
-                            onPress={() => {}}
+                            h={100}
+                            w={"100%"}
+                            mt="30"
+                            alignItems="center"
+                            p="7"
                         >
-                            Add to Cart
-                        </Button>
-                    </HStack>
-                </Box>
+                            <Text bold color="ocean.200" fontSize="17">
+                                {equipment.price}
+                            </Text>
+
+                            <Button
+                                w="149"
+                                h="43"
+                                justifyContent="center"
+                                alignItems="center"
+                                borderRadius="10px"
+                                shadow={5}
+                                bg="ocean.200"
+                                _pressed={{ bgColor: "ocean.100" }}
+                                onPress={() => {}}
+                            >
+                                Add to Cart
+                            </Button>
+                        </HStack>
+                    </Box>
+                </Animated.View>
             )}
         </>
     );
